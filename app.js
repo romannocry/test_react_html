1aa69435804f6dac5e59138cbd22d0c483e69b59
romannocry <roman.medioni@gmail.com>
2020-08-04 08:04:54 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,22 +3,18 @@
     const { browserHistory, Router, Route, IndexRoute, Switch, Link } = ReactRouterDOM;
     //const { button, Tab, Tabs } = ReactBootstrap;
     const { Select } = reactDropdownSelect;
-
+    const { Card } = ReactCardFlip;
 
     const render = ReactDOM.render;
     const html = htm.bind(createElement);
-
-//Router
-let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm',component:JoinTeamForm},{name:'ClickCounter',component:ClickCounter}]
-
-
 
     //data service
     const getTeams = async () => {
       try {
       const response = await fetch(`teams.json`);
       const Teams = await response.json();
-      console.log(Teams);
+      //console.log(Teams);
+      console.log("Getting teams from data service");
       return Teams;
       } catch (err) {
       console.log(err);
@@ -30,7 +26,7 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
 
     const [teams, setTeams] = useState([]);
     const componentIsMounted = useRef(true);
-  
+    console.log("loading Team List")
     useEffect(() => {
         getTeams()
         .then(response => {
@@ -47,24 +43,34 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
     }, []);
   
         return html`
-            <div className="team-container">
-                ${teams.map((team, index) => (
-                    html`<${Team} team=${team} key=${index}/>`
-                ))}
-            </div>`;
+        <table class="c-table">
+        <thead class="c-table__head">
+				<tr class="c-table__head-row">
+					<th class="c-table__head-cell u-text--center">Place</th>
+					<th class="c-table__head-cell">Driver</th>
+					<th class="c-table__head-cell">Wins</th>
+					<th class="c-table__head-cell u-text--right">Points</th>
+				</tr>
+			</thead>
+			<tbody>
+        ${teams.map((team, index) => (
+          html`<${Team} team=${team} key=${index}/>`
+      ))}
+      </tbody>
+      </table>
+  `;
     }
   
     //Team Card component
     const Team = props => {
       const [teamState, setTeamState] = useState(props);
-      console.log(teamState);
       return html`
-        <div onClick=${() => alert(teamState.team.joinID)} >
-          <p>
-            <strong>Name:</strong>
-            ${teamState.team.name}
-          </p>
-        </div>`;
+      <tr class="c-table__row" onClick=${() => alert(teamState.index)}>
+        <td class="c-table__cell c-table__cell--place u-text--center"><span class="c-place">test</span></td>
+        <td class="c-table__cell c-table__cell--name">${teamState.team.name}<br/><small>Team Captain</small></td>
+        <td class="c-table__cell c-table__cell--count"><small>1</small></td>
+        <td class="c-table__cell c-table__cell--points u-text--right"><strong>22</strong></td>
+      </tr>`;
     };
   
     //Actions
@@ -89,6 +95,7 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
       const { value, onChange, reset } = useInput('YourTeamName');
       const [members, setMembers] = useState({});
       const options = [{"id":1,"value":"Roman","label":"Roman"},{"id":2,"value":"joe","label":"joe"},{"id":3,"value":"bengi","label":"bengi"},];
+      console.log("loading Create Team form");
 
       const handleSubmit = (evt) => {
           evt.preventDefault();
@@ -99,10 +106,7 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
           //  value="";
           setMembers("");
       }
-      
-      const onChange2 = event =>{
-        console.log(event);
-      }
+ 
 
       return html`
       <div class="card">
@@ -116,7 +120,7 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
 
         <div class="form-group">
           <label for="teamName">Team Name</label>
-          <input type="text" class="form-control" id="teamName" aria-describedby="teamName" placeholder="${value}" onChange="${onChange}"/>
+          <input type="text" class="form-control" id="teamName" aria-describedby="teamName" placeholder="${value}" onChange="${onChange}" required/>
           <small id="teamHelp" class="form-text text-muted">Better be a cool name.</small>
         </div>
         <div class="form-group">
@@ -127,7 +131,15 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
           required
           multi
           onChange=${(values) => setMembers(values)}
-        />   
+        />
+        <!-- Switch with customized icons -->
+        <div class="switch switch-square"
+          data-on-label="<i class='fui-check'></i>"
+          data-off-label="<i class='fui-cross'></i>">
+          <input type="checkbox" />
+        </div>
+        <input type="checkbox" checked data-toggle="switch" />
+
         </div>
 
  
@@ -140,7 +152,8 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
 
     function JoinTeamForm(props) {
       const { value, onChange, reset } = useInput('Team secret ID');
-      
+      console.log("loading Join Team");
+
       const handleSubmit = (evt) => {
           evt.preventDefault();
           alert(`Joining Team ${value}`);
@@ -168,7 +181,7 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
 
     function AddMeToRandomTeam(props) {
       const { value, onChange, reset } = useInput('Team secret ID');
-      
+      console.log("loading AddMeToRandomTeam");
       const handleSubmit = (evt) => {
           evt.preventDefault();
           alert(`Joining Team ${value}`);
@@ -199,33 +212,83 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
       const [count, setCount] = useState(0);
       console.log("loading counter");
       return html`
+      <div>
+      <section id=timeline>
+      <h1>Rules & Timeline</h1>
+      <p class="leader">All cards must be the same height and width for space calculations on large screens.</p>
+      <div class="demo-card-wrapper">
+        <div class="demo-card demo-card--step1">
+          <div class="head">
+            <div class="number-box">
+              <span>01</span>
+            </div>
+            <h2><span class="small">Creating</span>Teams</h2>
+          </div>
+          <div class="body">
+            <p>Create a team or request to join one</p>
+          </div>
+        </div>
+    
+        <div class="demo-card demo-card--step2">
+          <div class="head">
+            <div class="number-box">
+              <span>02</span>
+            </div>
+            <h2><span class="small">Subtitle</span> Confidence</h2>
+          </div>
+          <div class="body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+          </div>
+        </div>
+    
+        <div class="demo-card demo-card--step3">
+          <div class="head">
+            <div class="number-box">
+              <span>03</span>
+            </div>
+            <h2><span class="small">Subtitle</span> Adaptation</h2>
+          </div>
+          <div class="body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+          </div>
+        </div>
+    
+        <div class="demo-card demo-card--step4">
+          <div class="head">
+            <div class="number-box">
+              <span>04</span>
+            </div>
+            <h2><span class="small">Subtitle</span> Consistency</h2>
+          </div>
+          <div class="body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+          </div>
+        </div>
+    
+        <div class="demo-card demo-card--step5">
+          <div class="head">
+            <div class="number-box">
+              <span>05</span>
+            </div>
+            <h2><span class="small">Subtitle</span> Conversion</h2>
+          </div>
+          <div class="body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+            <!--<img src="http://placehold.it/1000x500" alt="Graphic">-->
+          </div>
+        </div>
         
-        <div>
+      </div>
+    </section>
+            <div>
           <button onClick=${() => setCount(count + 1)}>
             Clicked ${count} times
           </button>
         </div>
+        </div>
       `;
     }
 
-    function Menu(){
-      
-      const [active,setActive] = useState({"label":"nav-home"})
-      
-      return html`
-        
-      <nav>
-      <div class="nav nav-tabs" id="nav-tab" role="tablist">
-        <a className=${ active.label == 'nav-home' ? 'nav-item nav-link active': 'nav-item nav-link' } onClick="${() => setActive({"label":"nav-home"})}" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-        <a className=${ active.label == 'nav-profile' ? 'nav-item nav-link active': 'nav-item nav-link' } onClick="${() => setActive({"label":"nav-profile"})}" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-        <a className=${ active.label == 'nav-contact' ? 'nav-item nav-link active': 'nav-item nav-link' } onClick="${() => setActive({"label":"nav-contact"})}" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
-      </div>
-    </nav>
-    <div class="tab-content" id="nav-tabContent">
-     
-    </div>
-      `;
-    }
     //Bottom of the tree function
     function App() {
       
@@ -238,10 +301,7 @@ let menu =[{name:'CreateTeamForm',component:CreateTeamForm},{name:'JoinTeamForm'
       
       const options = [{"id":1,"value":"Roman","label":"Roman"},{"id":2,"value":"joe","label":"joe"},{"id":3,"value":"bengi","label":"bengi"},];
 
-      const onChange2 = event =>{
-        console.log(event);
-      }
-      console.log(active.path);
+      //console.log(active.path);
       return (html`
       <nav>
       <div class="nav nav-tabs" id="nav-tab" role="tablist">
